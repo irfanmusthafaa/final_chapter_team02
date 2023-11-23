77de2d88f8b402f4cfd9e81c7b3e26fcbb95de9f
RafaniSalsabilah <rafanisalsa1235@gmail.com>
2023-11-23 23:22:47 +0700

    Register Page - Styling

--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Beranda } from "../pages/Beranda";
+import { Register } from "../pages/Register";
 
 export const RouterList = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Beranda />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
