ec31e2cd6b0eeff1487b97efdeff7b5d6ff9325c
Irfan Mustafa <irfanmustafa2307@gmail.com>
2023-11-26 13:05:14 +0700

    slicing button filter category

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import { RouterList } from "./routes/RouterList";
-import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterList />
-    </ThemeProvider>
+    <RouterList />
   </React.StrictMode>
 );
