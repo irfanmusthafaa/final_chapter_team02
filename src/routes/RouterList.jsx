7eba2a5a692bf7139beaa0130fe7fe59da093e2c
Muhammad Aji <muhammadajisolehudin@gmail.com>
2023-11-26 17:40:13 +0700

    slicing page kelas saya -> topik kelas

--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Beranda } from "../pages/Beranda";
 import { KelasBerjalan } from "../pages/KelasSaya/KelasBerjalan";
+import { TopikKelas } from "../pages/KelasSaya/TopikKelas";
 
 export const RouterList = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Beranda />}></Route>
-        <Route path="/kelasSaya" element={<KelasBerjalan />}></Route>
+        <Route path="/kelasSaya/KelasBerjalan" element={<KelasBerjalan />}></Route>
+        <Route path="/KelasSaya/TopikKelas" element={<TopikKelas />}></Route>
       </Routes>
     </BrowserRouter>
   );
