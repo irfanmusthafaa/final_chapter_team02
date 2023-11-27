f6e36b3b0519f4f4352a8b4fcbe81edab3f7d71d
Muhammad Aji <muhammadajisolehudin@gmail.com>
2023-11-27 18:20:04 +0700

    perbaikan button navigasi

--- src/assets/components/button/buttton_navigasi/ButtonNav.jsx
+++ src/assets/components/button/buttton_navigasi/ButtonNav.jsx
@@ -5,7 +5,7 @@ export const NavButton = ( {button_text, onClick, isActive} ) => {
   return (
     <button
       size="large"
-      className={`bg-white py-2 px-20 cursor-pointer text-purple-700 font-bold rounded-full hover:bg-purple-900 hover:text-white border-0 shadow-sm transition-transform transform hover:scale-105 focus:outline-none  ${isActive ? 'bg-purple-900 text-white' : ''}`}
+      className={`py-3 px-20 cursor-pointer text-purple-700 font-bold rounded-xl hover:bg-purple-900 hover:text-white border-0 shadow-sm transition-transform transform hover:scale-105 focus:outline-none  ${isActive ? 'bg-purple-900 text-white' : 'bg-white'}`}
       onClick={onClick}
     >
       {button_text}
