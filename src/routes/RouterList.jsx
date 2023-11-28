0415ae4d2b718d9c3dfbefe1065eae13927d396b
RafaniSalsabilah <rafanisalsa1235@gmail.com>
2023-11-28 10:53:34 +0700

    OTP Page

--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Beranda } from "../pages/Beranda";
 import { Register } from "../pages/Register";
+import { OTP } from "../pages/OTP";
 
 export const RouterList = () => {
   return (
@@ -9,6 +10,7 @@ export const RouterList = () => {
       <Routes>
         <Route path="/" element={<Beranda />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/otp" element={<OTP />}></Route>
       </Routes>
     </BrowserRouter>
   );
