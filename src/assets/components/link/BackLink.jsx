5f56ec931e645509aa637f5cb89308900349e155
Muhammad Aji <muhammadajisolehudin@gmail.com>
2023-11-30 00:46:47 +0700

    melengkapi halaman detail kelas page

---
+++ src/assets/components/link/BackLink.jsx
@@ -0,0 +1,16 @@
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const BackLink = () => {
+  return (
+    <div className='flex gap-1'>
+      <FontAwesomeIcon icon={faArrowLeft} />
+      <Link to="#" onClick={() => window.history.back()} className="text-black font-bold no-underline hover:underline cursor-pointer">
+        Lihat Kelas Lainnya
+      </Link>
+    </div>
+    
+  )
+}
---
+++ src/pages/DetailKelas/MateriBelajar.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+
+export const MateriBelajar = () => {
+  return (
+    <div>
+    <div class="w-[400px] h-[718px] px-6 py-5 bg-white rounded-2xl shadow flex-col justify-center items-center gap-2.5 inline-flex">
+  <div class="h-[678px] flex-col justify-center items-center gap-2.5 flex">
+    <div class="self-stretch justify-start items-center gap-2.5 inline-flex">
+      <div class="grow shrink basis-0 text-black text-xl font-bold font-['Montserrat'] leading-[14px]">Materi Belajar</div>
+      <div class="h-[18px] px-[5px] justify-end items-center gap-[5px] flex">
+        <div class="w-4 h-4 relative"></div>
+        <div class="w-[162px] h-[18px] relative">
+          <div class="w-[150px] h-4 left-[16px] top-[1px] absolute bg-zinc-300 rounded-[25px]"></div>
+          <div class="w-[34px] h-4 left-[14.50px] top-[1px] absolute bg-indigo-600 rounded-[25px]"></div>
+          <div class="w-[148px] h-[15px] left-[18px] top-[1px] absolute text-white text-[10px] font-normal font-['Montserrat'] leading-[14px]">   10% complete</div>
+        </div>
+      </div>
+    </div>
+    <div class="py-[5px] flex-col justify-start items-start flex">
+      <div class="py-2.5 justify-center items-center inline-flex">
+        <div class="w-[228px] h-[22px] text-indigo-600 text-xs font-extrabold font-['Montserrat'] leading-9">Chapter 1 - Pendahuluan</div>
+        <div class="w-[76px] h-[22px] text-right text-blue-400 text-xs font-extrabold font-['Montserrat'] leading-9">60 Menit</div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">1</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Tujuan Mengikuti Kelas Design System</div>
+          <img class="w-[17.60px] h-[18px]" src="https://via.placeholder.com/18x18" />
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">2</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Pengenalan Design System</div>
+          <img class="w-[17.60px] h-[18px]" src="https://via.placeholder.com/18x18" />
+        </div>
+      </div>
+      <div class="h-14 py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">3</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Contoh Dalam Membangun Design System</div>
+          <img class="w-[17.60px] h-[18px]" src="https://via.placeholder.com/18x18" />
+        </div>
+      </div>
+    </div>
+    <div class="py-[5px] flex-col justify-start items-start flex">
+      <div class="py-2.5 justify-center items-center inline-flex">
+        <div class="w-[221px] h-[22px] text-indigo-600 text-xs font-extrabold font-['Montserrat'] leading-9">Chapter 2 - Memulai Desain</div>
+        <div class="w-[76px] h-[22px] text-right text-blue-400 text-xs font-extrabold font-['Montserrat'] leading-9">120 Menit</div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">4</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Color Palette</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">5</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Typography, Layout dan Grid</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">6</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Components</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">7</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Components</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">8</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Components</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">9</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Components</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">10</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Components</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+      <div class="h-[52px] py-[5px] flex-col justify-start items-start gap-[5px] flex">
+        <div class="w-[352px] justify-start items-center gap-2.5 inline-flex">
+          <div class="w-9 h-9 relative">
+            <div class="w-9 h-9 left-0 top-0 absolute bg-indigo-50 rounded-full border-2 border-indigo-50"></div>
+            <div class="w-[12.52px] left-[12px] top-[11px] absolute text-center text-indigo-950 text-[10px] font-bold font-['Montserrat'] leading-[14px]">11</div>
+          </div>
+          <div class="w-[251px] text-black text-opacity-80 text-xs font-normal font-['Montserrat'] leading-tight">Membuat Asset</div>
+          <div class="w-6 h-6 relative"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
