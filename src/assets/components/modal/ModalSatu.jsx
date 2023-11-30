cdebd14dc01c4f59ed4b7a30eda4f961044b2a1b
Irfan Mustafa <irfanmustafa2307@gmail.com>
2023-11-30 22:52:21 +0700

    update modalSatu component

--- src/assets/components/modal/ModalSatu.jsx
+++ src/assets/components/modal/ModalSatu.jsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
-import { CustomButtonSatu } from '../assets/components/button/CustomButtonSatu';
-import next from '../assets/images/icon/carbon_next-filled.svg'
-import { Divider } from 'antd';
-import img from "../assets/images/kursus.png";
-import { CardModal } from '../assets/components/card/CardModal';
-
+import React, { useState } from "react";
+import { CustomButtonSatu } from "../button/CustomButtonSatu";
+import next from "../../images/icon/carbon_next-filled.svg";
+import { Divider } from "antd";
+import img from "../../images/kursus.png";
+import { CardModal } from "../../components/card/CardModal";
 
 const ModalSatu = ({ isOpen, onClose, children }) => {
-  const overlayClasses = isOpen ? 'fixed inset-0 bg-black opacity-50' : 'hidden';
-  const modalClasses = isOpen ? 'fixed inset-1/2 w-[500px] h-[400px] transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-xl' : 'hidden';
+  const overlayClasses = isOpen ? "fixed inset-0 bg-black opacity-50" : "hidden";
+  const modalClasses = isOpen ? "fixed inset-1/2 w-[500px] h-[400px] transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-xl" : "hidden";
 
   return (
     <div>
       <div className={overlayClasses} onClick={onClose}></div>
-      <div className={modalClasses}>
-        {children}
-      </div>
+      <div className={modalClasses}>{children}</div>
     </div>
   );
 };
@@ -33,13 +30,13 @@ const App = () => {
 
   const contentData = {
     img: img,
-    title: 'UI/UX Design',
-    author: 'Angela Doe',
-    deskripsi: 'ini mempelajari ui/ux fundamental',
-    rating: '4.5', // Change this to your actual rating
-    level: 'Intermediate',
+    title: "UI/UX Design",
+    author: "Angela Doe",
+    deskripsi: "ini mempelajari ui/ux fundamental",
+    rating: "4.5", // Change this to your actual rating
+    level: "Intermediate",
     modules: 8,
-    durasi: '12',
+    durasi: "12",
   };
 
   return (
@@ -50,18 +47,15 @@ const App = () => {
 
       <ModalSatu isOpen={isModalOpen} onClose={closeModal}>
         <div className="">
-            <div className="font-bold mb-4 text-center">
-                <p>Selangkah Lagi Menuju</p>
-                <p className='text-purple-900'>Menuju Premium</p>
-                
-            </div>
+          <div className="font-bold mb-4 text-center">
+            <p>Selangkah Lagi Menuju</p>
+            <p className="text-purple-900">Menuju Premium</p>
+          </div>
 
-
-            
-            <CardModal {...contentData}/>
-            <div className="flex items-center justify-center mt-8">
-                <CustomButtonSatu button_text="Beli Sekarang" iconPath={next} onClick={closeModal}/>
-            </div>
+          <CardModal {...contentData} />
+          <div className="flex items-center justify-center mt-8">
+            <CustomButtonSatu button_text="Beli Sekarang" iconPath={next} onClick={closeModal} />
+          </div>
         </div>
       </ModalSatu>
     </div>
