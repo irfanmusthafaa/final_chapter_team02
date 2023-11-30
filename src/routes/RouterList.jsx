846b3e1f06e050b514e612fd90fcd0729446ba38
Irfan Mustafa <irfanmustafa2307@gmail.com>
2023-11-30 21:37:33 +0700

    update router

--- src/routes/RouterList.jsx
+++ src/routes/RouterList.jsx
@@ -12,11 +12,8 @@ import { AkunRiwayatPembayaran } from "../pages/AkunRiwayatPembayaran";
 import { AdminLogin } from "../pages/Admin/AdminLogin";
 import { AdminDashboard } from "../pages/Admin/AdminDashboard";
 import { AdminKelolaKelas } from "../pages/Admin/AdminKelolaKelas";
-import { Register } from "../pages/Register";
-import { Login } from "../pages/Login";
 import { ResetPass } from "../pages/ResetPass";
 import { OTP } from "../pages/OTP";
-
 
 export const RouterList = () => {
   return (
@@ -24,9 +21,9 @@ export const RouterList = () => {
       <Routes>
         <Route path="/" element={<Beranda />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/detail-pembayaran" element={<DetailPembayaran/>}></Route>
-        <Route path="/sukses-pembayaran" element={<SuksesPembayaran/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/detail-pembayaran" element={<DetailPembayaran />}></Route>
+        <Route path="/sukses-pembayaran" element={<SuksesPembayaran />}></Route>
         <Route path="/notifikasi" element={<Notifikasi />}></Route>
         <Route path="/profil" element={<AkunProfil />}></Route>
         <Route path="/ubah-password" element={<AkunUbahPassword />}></Route>
@@ -35,8 +32,8 @@ export const RouterList = () => {
         <Route path="/admin/dashboard" element={<AdminDashboard />}></Route>
         <Route path="/admin/kelas" element={<AdminKelolaKelas />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/reset-password" element={<ResetPass/>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/reset-password" element={<ResetPass />}></Route>
         <Route path="/otp" element={<OTP />}></Route>
       </Routes>
     </BrowserRouter>
