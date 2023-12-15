b269de2540f58432e47002ba2bd1878ee51aafa8
Irfan Mustafa <irfanmustafa2307@gmail.com>
2023-12-15 21:18:07 +0700

    update utils http

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,20 +6,21 @@ const http = axios.create({
   timeout: 30000,
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json",
+    // "Content-Type": "application/json",
   },
 });
 
 http.interceptors.request.use((config) => {
-  // Dapatkan tipe konten dari konfigurasi permintaan
-  const contentType = config.headers["Content-Type"];
-
-  // Jika tipe konten adalah "multipart/form-data", biarkan seperti itu
-  if (contentType && contentType.includes("multipart/form-data")) {
-    // Tidak perlu menambahkan "Content-Type" di sini
+  if (config.data instanceof FormData) {
+    config.headers = {
+      ...config.headers,
+      "Content-Type": "multipart/form-data",
+    };
   } else {
-    // Jika bukan "multipart/form-data", atur "Content-Type" menjadi "application/json"
-    config.headers["Content-Type"] = "application/json";
+    config.headers = {
+      ...config.headers,
+      "Content-Type": "application/json",
+    };
   }
 
   config.headers = {
